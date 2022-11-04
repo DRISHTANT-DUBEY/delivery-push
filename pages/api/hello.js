72c0a1b9891f3092f20194c4ea6de02f20376f8e
Drishtant Dubey <drishtantkrishna@gmail.com>
2022-11-04 21:27:27 +0530

    First Code Commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import { NextApiRequest, NextApiResponse } from "next"
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  //next js api response sending of delivery orders in json
+  res.status(200).json({ product_name: "", product_price: "", product_quantity: "", product_total: "", product_image: "", product_id: "",  shipping_id:"", route_map:["",""]})
+  res.status(200).json({username: "Drishtant Dubey"})
+
 }
