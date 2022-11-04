71731a464ef6f8a9adb0ea826b4d3ad2cf1e8fd7
Drishtant Dubey <drishtantkrishna@gmail.com>
2022-11-04 21:34:11 +0530

    Updated API

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,6 +3,6 @@ import { NextApiRequest, NextApiResponse } from "next"
 export default function handler(req, res) {
   //next js api response sending of delivery orders in json
   res.status(200).json({ product_name: "", product_price: "", product_quantity: "", product_total: "", product_image: "", product_id: "",  shipping_id:"", route_map:["",""]})
-  res.status(200).json({username: "Drishtant Dubey"})
+  
 
 }
